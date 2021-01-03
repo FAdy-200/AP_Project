8e1f0bdc0caecbe727d83a7687fc85d0f738076b
FAdy200 <abuyehia200@gmail.com>
2021-01-03 23:41:11 +0200

    dividing the workload and assigning the work

--- TESTING/TESTING.py
+++ TESTING/TESTING.py
@@ -1,21 +1,24 @@
 import pygame
 import sys
 from PIL import Image
+
 # image = Image.open("Webp.net-resizeimage (15).png")
 # mode = image.mode
 # size = image.size
 # data = image.tobytes()
 #
 # py_image = pygame.image.fromstring(data, size, mode)
-image = pygame.image.load("ps\BG.png")
+image = pygame.image.load("~\ps\BG.png")
 w, h = image.get_size()
-image2 = pygame.image.load("ps\Big_Arrow.png")
+image2 = pygame.image.load("~\ps\Big_Arrow.png")
 # image4 = pygame.image.load("11sdasd.png")
 # image6 = pygame.image.load("11sdasd2.png")
 # image8 = pygame.image.load("11sdasd2.png")
 sc = pygame.display.set_mode((w, h))
 mouse = pygame.mouse.get_pos()
 i = 0
+checkw = False
+checks = False
 image3 = pygame.transform.rotate(image2, i)
 # image5 = pygame.transform.rotate(image4, i)
 # image7 = pygame.transform.rotate(image6, i)
@@ -24,10 +27,25 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                checkw = True
+            if event.key == pygame.K_s:
+                checks = True
+        if event.type == pygame.KEYUP:
+            checkw = False
+            checks = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             # image = image.rotate(10)
             print(mouse)
-    i -= 2
+    if checkw:
+        i -= 2
+    else:
+        checkw = False
+    if checks:
+        i += 2
+    else:
+        checks = False
     image3 = pygame.transform.rotate(image2, i)
     mouse = pygame.mouse.get_pos()
     # image5 = pygame.transform.rotate(image4, i)
@@ -42,7 +60,7 @@ while True:
     # rect4 = image9.get_rect()
     # rect4.center = 430, 404
     sc.fill((245, 245, 245))
-    sc.blit(image,(0,0))
+    sc.blit(image, (0, 0))
     sc.blit(image3, rect)
     # sc.blit(image5,rect2)
     # sc.blit(image7, rect3)
---
+++ main.py
@@ -0,0 +1,51 @@
+import fileInput
+import userInput
+import pygame
+import sys
+
+
+class startingScreen:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((600, 200))
+        self.font = pygame.font.Font('freesansbold.ttf', 20)
+        self.mouse = pygame.mouse.get_pos()
+
+    def render(self):
+        main_text = self.font.render("Please choose which mode you want:", True, (0, 0, 0))
+        user_text = self.font.render("User Driven", True, (0, 0, 0))
+        file_text = self.font.render("File Driven", True, (0, 0, 0))
+        main_text_rect = main_text.get_rect()
+        main_text_rect.center = 300, 40
+        user_text_rect = user_text.get_rect()
+        user_text_rect.center = 120, 100
+        file_text_rect = file_text.get_rect()
+        file_text_rect.center = 500, 100
+        self.screen.blit(main_text, main_text_rect)
+        self.screen.blit(user_text, user_text_rect)
+        self.screen.blit(file_text, file_text_rect)
+
+    def start(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if 110 > self.mouse[1] > 85:
+                        if 180 > self.mouse[0] > 60:
+                            return "USER"
+                        elif 555 > self.mouse[0] > 445:
+                            return "FILE"
+            self.mouse = pygame.mouse.get_pos()
+            self.screen.fill((245, 245, 245))
+            self.render()
+            pygame.display.flip()
+
+
+s = startingScreen()
+mode = s.start()
+if mode == "USER":
+    dash_board = userInput.DashBoardUserInput()
+else:
+    dash_board = fileInput.DashBoardFileInput()
+dash_board.start()
