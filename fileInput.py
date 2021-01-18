d99b99375927cb7766d7402025ec19c66be8ec93
MennaEwas <61921636+MennaEwas@users.noreply.github.com>
2021-01-19 00:45:35 +0200

    Update fileInput.py

--- fileInput.py
+++ fileInput.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 class DashBoardFileInput:
     def __init__(self):
+        self.i = 0 
         self.bg = pygame.image.load("Data/BG.png")
         self.width, self.height = self.bg.get_size()
         self.screen = pygame.display.set_mode((self.width, self.height))  # screen to render thing into
@@ -19,7 +20,7 @@ class DashBoardFileInput:
         self.seat_belt = pygame.image.load("Data/SeatBelt.png")
         self.mouse = pygame.mouse.get_pos()  # print this when you need to find the mouse position on screen
         self.battery_flag = False
-        self.brakes_flag = False
+        #self.brakes_flag = False
         self.engine_flag = False
         self.flasher_flag = False
         self.signal_left_flag = False
@@ -27,7 +28,7 @@ class DashBoardFileInput:
         self.seat_belt_flag = False
         self.big_arrow_angle = 0
         self.small_arrow_angle = 0
-        self.data = pd.read_csv("FILENAMEHERE")  # put the file in the Data directory and access it here
+        self.data = pd.read_excel('data.xlsx')  # put the file in the Data directory and access it here
 
     def __check_for_events(self):
         for event in pygame.event.get():
@@ -40,17 +41,41 @@ class DashBoardFileInput:
         Change all the required flags to be used in the render function
         :return:
         """
-        pass
+        while self.i < 100:
+            acc = self.data['Acceleration'][self.i]  #number not flag
+            fuel = self.data['Fuel'][self.i] #number not flag
+            self.battery_flag = self.data['Battery'][self.i]
+            self.seat_belt_flag = self.data['Set Belt'][self.i]
+            self.flasher_flag = self.data['Alart'][self.i]
+            self.engine_flag = self.data['Engine'][self.i]
+            self.signal_left_flag = self.data['Left'][self.i]
+            self.signal_right_flag = self.data['Right'][self.i]
+            
+            
+            
 
     def __render(self):
         """
         renders needed images based on the input
         :return:
         """
-        pass
-
+        if self.battery_flag:
+                self.screen.blit(self.battery , (0,0))
+            if self.seat_belt_flag:
+                self.screen.blit(self.seat_belt, (0,0))
+            if self.flasher_flag:
+                self.screen.blit(self.flasher , (0,0))
+            if self.engine_flag:
+                self.screen.blit(self.engine, (0,0))
+            if self.signal_left_flag:
+                self.screen.blit( self.signal_left, (0,0))
+            if self.signal_right_flag:
+                self.screen.blit(self.signal_right , (0,0))
+            self.i += 1
+      
+            
     def __check_for_warning(self):
-        """
+        
         MY JOB LEAVE IT ALONE
         :return:
         """
